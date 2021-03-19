6f202d38faecd505c8d7e0b015d50cb0ebac0aeb
swit01 <swit01@DESKTOP-MBTOPGP>
2021-03-19 11:21:25 +0900

    Mapper XML파일 설정

---
+++ MybatisProject/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -0,0 +1,49 @@
+package com.springbook.biz.board.impl;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.util.SqlSessionFactoryBean;
+
+//DAO(Data Access Object)
+public class BoardDAO {
+	// JDBC 관련 변수
+	private SqlSession mybatis;
+
+	public BoardDAO() {
+		mybatis = SqlSessionFactoryBean.getSqlSessionInstance();
+	}
+	
+	// CRUD 기능의 메소드 구현
+	// 글 등록
+	public void insertBoard(BoardVO vo) {
+		mybatis.insert("BoardDAO.insertBoard", vo);
+		mybatis.commit();
+	}
+	
+
+	// 글 수정
+	public void updateBoard(BoardVO vo) {
+		mybatis.update("BoardDAO.updateBoard",vo);
+		mybatis.commit();
+	}
+
+	// 글 삭제
+	public void deleteBoard(BoardVO vo) {
+		mybatis.delete("BoardDAO.deleteBoard",vo);
+		mybatis.commit();
+	}
+
+	// 글 상세조회
+	public BoardVO getBoard(BoardVO vo) {
+		return (BoardVO) mybatis.selectOne("BoardDAO.getBoard", vo);
+	}
+
+	// 글 목록 조회
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		return mybatis.selectList("BoardDAO.getBoardList",vo);
+	}
+
+}
